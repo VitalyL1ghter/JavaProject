c76e81661ac49c327546bf094ed5c69124ec5d74
l1 <stalerk-kzn@mail.ru>
2023-11-08 19:57:45 +0300

    ДЗ №4 задание 1

---
+++ src/ru/innopolis/homework04/task1.java
@@ -0,0 +1,24 @@
+package ru.innopolis.homework04;
+
+import java.util.Scanner;
+import java.lang.String;
+
+public class task1 {
+
+    public static void main(String[] args) {
+
+        String alph = "qwertyuiopasdfghjklzxcvbnm";
+        Scanner keyb = new Scanner(System.in);
+        System.out.println("Введите букву английского алфавита: ");
+        String x = keyb.next();
+         int index1 = alph.indexOf(x);
+         if(index1==0) {
+             System.out.println("Соседняя буква слева от " + x + " - " + alph.charAt(25));
+         } else {
+              System.out.println("Соседняя буква слева от " + x + " - " + alph.charAt(index1-1));
+        }
+
+    }
+
+}
+
