bb2c5dc535dec2d140f4127fcd3080563f8fd439
l1 <stalerk-kzn@mail.ru>
2023-11-08 19:58:20 +0300

    ДЗ №4 задание 3

---
+++ src/ru/innopolis/homework04/task3.java
@@ -0,0 +1,28 @@
+package ru.innopolis.homework04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class task3 {
+    public static void main(String[] args) {
+        Scanner word = new Scanner(System.in);
+        System.out.println("Введите 1 слово на английском языке: ");
+        String w1 = word.next();
+        System.out.println("Введите 2 слово на английском языке: ");
+        var w2 = word.next();
+        System.out.println("Введенные слова: " + w1 + " " + w2);
+        char[] charArray = w1.toCharArray();
+        Arrays.sort(charArray);
+        var sortedw1 = new String(charArray);
+        char[] charArray1 = w2.toCharArray();
+        Arrays.sort(charArray1);
+        var sortedw2 = new String(charArray1);
+
+        System.out.println("Результат сортировки: " + sortedw1 + " " + sortedw2);
+    }
+
+}
+
+
+
+
