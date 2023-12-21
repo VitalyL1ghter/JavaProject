71f15009c680066f68aef1f859c965a5eaac45fa
l1 <stalerk-kzn@mail.ru>
2023-12-21 11:55:51 +0300

    Принято замечание. Исправленно, название класса с большой буквы.

--- src/ru/innopolis/homework03/App.java
+++ src/ru/innopolis/homework03/App.java
@@ -2,14 +2,14 @@ package ru.innopolis.homework03;
 
 public class App {
     public static void main(String[] args) {
-    televisor Sony = new televisor();
+    Televisor Sony = new Televisor();
         Sony.setColor("black");
         Sony.setName("Sony");
         Sony.setDiag(19);
         System.out.println(Sony);
         System.out.println(Sony.chanell()); // Канал
 
-    televisor Sharp = new televisor("Sharp", 24,"white");
+    Televisor Sharp = new Televisor("Sharp", 24,"white");
 
         System.out.println(Sharp);
        System.out.println(Sharp.chanell());  // Канал
