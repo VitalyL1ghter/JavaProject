ef4126c41b33ccc49b507a1183ed2b90f4ea2e3d
l1 <stalerk-kzn@mail.ru>
2023-12-21 12:34:52 +0300

    Принято замечание. Исправленно, добавлен пустой конструктор в классы, переименнованы переменые циклов.

--- src/ru/innopolis/homework06/App.java
+++ src/ru/innopolis/homework06/App.java
@@ -11,36 +11,35 @@ public class App {
         products[5] = new Product("Мороженное", 200);
         products[6] = new Product("Макароны", 800);
         System.out.println("Список продуктов с ценами :");
-        for (Product s : products) {
-            System.out.println(s);
+        for (Product productList : products) {
+            System.out.println(productList);
         }
 
         Person[] newPerson = new Person[5];
-        int x;
         newPerson[0] = new Person("Павел Андреевич", 10000);
         newPerson[1] = new Person("Анна Петровна", 2000);
         newPerson[2] = new Person("Борис", 10);
         newPerson[3] = new Person("Женя", 0);
         newPerson[4] = new Person("Света", -3);
         System.out.println("Список покупателей : ");
-        for (Person d : newPerson) {
-            System.out.println(d);
+        for (Person personList : newPerson) {
+            System.out.println(personList);
         }
 
-        for (Person d : newPerson) {
-            System.out.println("Покупает: " + d.getName());
-            if (d.getCash() < 0) {
+        for (Person personList : newPerson) {
+            System.out.println("Покупает: " + personList.getName());
+            if (personList.getCash() < 0) {
                 System.out.println("Отрицательное число денег");
                 continue;
             }
-            if (d.getCash() == 0) {
+            if (personList.getCash() == 0) {
                 System.out.println("Нет денег");
                 continue;
             }
             try {
                 for (int i = 0; i < 3; i++) {
                     Product product = products[(int) (Math.random() * products.length)];
-                    d.byuProduct(product);
+                    personList.byuProduct(product);
                     System.out.println("Купил: " + product.getProductName());
                 }
             } catch (Exception ex) {
